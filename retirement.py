94ad130d6470364e8fcd993c38fd08a9d8b2f149
jgn46 <jgn46@msstate.edu>
2016-06-22 16:23:51 -0500

    Update retirement.py

--- retirement.py
+++ retirement.py
@@ -3,19 +3,37 @@ import math #imported to use floor(()
 
 # Function to calucate age to savings goal
 def calucate_years_to_goal (age, annual_sal, percentage, goal):
-    putback = (percentage/100) * annual_sal* 2
-    years = goal/putback
-    age_to_goal = years + age
-    age_to_goal = math.floor(age_to_goal)
-    
-    #IF age is equal or over 100
-    if age_to_goal >= 100:
-        print("Goal will not be reach in a lifetime")
-        print("Your age when goal is reached")
-        return age_to_goal
-    
-    else:
-        print("Your age when goal is reached:")
-        return  age_to_goal
+    x = False
 
+    try:
+        age = int(age)
+        annual_sal = int(annual_sal)
+        percentage = int(percentage)
+        goal = int(goal)
+
+    except ValueError:
+        print("")
+        print ("YOUR INPUT IS INVALID!")
+        age_to_goal = "Invalid"
+        x = True
+
+    else:
+        if (x == True):
+            return age_to_goal
+
+        putback = (percentage/100) * annual_sal* 2
+        years = goal/putback
+        age_to_goal = years + age
+        age_to_goal = math.floor(age_to_goal)
+
+        #IF age is equal or over 100
+
+        if age_to_goal >= 100:
+        
+            print("\nGOAL WILL NOT BE REACHED IN A LIFETIME")
+            print("Your age when goal is reached:")
+            return age_to_goal
+        else:
+            print("\nYour age when goal is reached:")
+            return age_to_goal
 
